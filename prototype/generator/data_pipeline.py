abd10a6efbc77c94c116b6452e5fedbac5f693c7
Scrashdemix <alexander.maisch@web.de>
2023-04-26 16:22:05 +0200

    Added feature and training pipeline

--- prototype/generator/data_pipeline.py
+++ prototype/generator/data_pipeline.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from generator.utils import write_pipeline_file
+from generator.utils import write_pipeline_file, dataset_wrapper
 
 
 class DataPipeline:
@@ -7,33 +7,31 @@ class DataPipeline:
     def __init__(self, config: dict, data_pipeline_dir: Path) -> None:
         '''
         Params:
-        pipeline_dir: path to directory called 'data_pipeline'
+        data_pipeline_dir: path to directory called 'data_pipeline'
         '''
-        if ('nodes' in config.keys()):
-            raise RuntimeError(
-                "There should not be any nodes configuration in the pipelines config at time of initialization."
-            )
         self.config = config
         self.data_pipeline_dir = data_pipeline_dir
+        self.additional_datasets = []
+        self.next_datasets = []  # names of datasets for next pipelines
         self._structure()
         self._write_files()
 
     def get_config(self):
         return self.config
     
+    def get_additional_datasets(self):
+        return self.additional_datasets
+    
+    def get_next_datasets(self):
+        return self.next_datasets
+    
     def _structure(self):
         nodes_list = []
-        nodes_list.append(
-            self.Nodes.node_join_datasets(
-                dataset_names=[ds['name'] for ds in self.config['datasets']],
-                output_name='joined_dataset'
-            ))
-        self.config['nodes'] = nodes_list
-        self.config['datasets'].append({
-            'name': 'joined_dataset',
-            'type': 'csv',
-            'filepath': 'data/02_intermediate/joined_data.csv',
-        })
+        node_jd, dataset_jd = self.Nodes.node_join_datasets(dataset_names=[ds['name'] for ds in self.config['datasets']])
+        nodes_list.append(node_jd)
+        self.config['nodes'].extend(nodes_list)
+        self.config['datasets'].append(dataset_jd)
+        self.next_datasets.append(dataset_jd['name'])
     
     def _write_files(self):
         nodes_list = self.config['nodes']
@@ -43,18 +41,19 @@ class DataPipeline:
         write_pipeline_file(self.data_pipeline_dir, nodes_list)
 
     class Nodes:
-        def node_join_datasets(dataset_names: list, output_name: str):
+        def node_join_datasets(dataset_names: list):
+            output_dataset = dataset_wrapper(name='joined_dataset', type='csv', filepath='data/02_intermediate/joined_data.csv')
             code = """import pandas as pd
 from functools import reduce
 
 
 def join_datasets(*datasets) -> pd.DataFrame:
-    return reduce(lambda left, right: pd.merge(left, right), datasets)
+    return [reduce(lambda left, right: pd.merge(left, right), datasets)]
 """
             return {
                 'func': 'join_datasets',
                 'name': 'Join_datasets',
                 'code': code,
                 'inputs': dataset_names,
-                'outputs': [output_name]
-            }
+                'outputs': [output_dataset['name']]
+            }, output_dataset
--- prototype/generator/generator.py
+++ prototype/generator/generator.py
@@ -3,6 +3,8 @@ from pathlib import Path
 from generator.utils import generate_pipeline_file_structure
 from generator.gen_data_catalog import update_data_catalog
 from generator.data_pipeline import DataPipeline
+from generator.feature_pipeline import FeatureEngineering
+from generator.training_pipeline import TrainingPipeline
 
 # structure:
 #   data pipeline
@@ -16,10 +18,18 @@ from generator.data_pipeline import DataPipeline
 
 def generate(config: dict, root_dir_path: str) -> None:
     root_dir = Path(root_dir_path)
+    config['nodes'] = []
     # Data pipeline
     data_pipeline_dir = generate_pipeline_file_structure(root_dir, 'data_pipeline')
-    data_pipe = DataPipeline(config=config, data_pipeline_dir=data_pipeline_dir)
+    data_pipe = DataPipeline(config, data_pipeline_dir)
     config['datasets'] = data_pipe.get_config()['datasets']
+    # Feature engineering pipeline
+    feature_pipeline_dir = generate_pipeline_file_structure(root_dir, 'feature_pipeline')
+    feature_pipe = FeatureEngineering(config, root_dir, feature_pipeline_dir, last_datasets=data_pipe.get_next_datasets())
+    # Training pipeline
+    training_pipeline_dir = generate_pipeline_file_structure(root_dir, 'training_pipeline')
+    training_pipe = TrainingPipeline(config, root_dir, training_pipeline_dir, last_datasets=feature_pipe.get_next_datasets())
+    config['datasets'].append(training_pipe.get_next_datasets()[0])
     # Data catalog
     update_data_catalog(config['datasets'], root_directory=root_dir)
     # Data pipeline
